5034f42102454ed30698829ecacf8470242ec737
HenriqueFaquim <henriquefa99@gmail.com>
2023-10-09 18:56:00 -0300

    produção1

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
 function routes(app) {
+    app.use('/',require('./routes/index.js'));
     app.use('/users', require('./routes/users.js'));
     app.use('/cont', require('./routes/cont.js'));
     return;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,9 @@
 var express = require('express');
+const ConnectToDB = require('../middlewares/ConnectToDB');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', ConnectToDB, function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
